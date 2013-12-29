82bb4d36c2a0022a015e5a131d2467e96c253b14
ttoti <tomas.hernandez@ieee.org>
2013-12-29 11:03:04 -0800

    Initial commit

---
+++ AlienShooter/src/com/tomashernandez/alienshooter/Bullet2D.java
@@ -0,0 +1,9 @@
+package com.tomashernandez.alienshooter;
+
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Bullet2D {
+	public Vector2 slope;
+	public Rectangle rect;
+}
