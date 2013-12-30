aa0570b00fc0bd7fc55b0f1e3425b415cba10b3c
Tomas Hernandez <tomas.hernandez@ieee.org>
2013-12-29 19:09:04 -0800

    Removed unnecessary XML and some code from main

--- AlienShooter/src/com/tomashernandez/alienshooter/MainActivity.java
+++ AlienShooter/src/com/tomashernandez/alienshooter/MainActivity.java
@@ -7,16 +7,7 @@ public class MainActivity extends AndroidApplication {
 	@Override	
 	public void onCreate (android.os.Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initialize(new Game(), false);		// initialize a new instance of your Game class
+		initialize(new Game(), true);		// initialize a new instance of your Game class
 	}
 	
-	/*
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	*/
-
 }
